79a62be6611280914a3867e6dbaa4fe0e2c77155
Arthur Zhuravetskiy <58107406+arthurCodename@users.noreply.github.com>
2021-12-13 22:53:59 +0100

    Add files via upload

---
+++ BinarySearchTree.py
@@ -0,0 +1,106 @@
+class Node:
+    """Klasa reprezentująca węzeł drzewa binarnego."""
+
+    def __init__(self, data=None, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.data)
+
+    def insert(self, node):
+        if node.data < self.data:   # mniejsze na lewo
+            if self.left:
+                self.left.insert(node)
+            else:
+                self.left = node
+        else:   # większe lub równe na prawo
+            if self.right:
+                self.right.insert(node)
+            else:
+                self.right = node
+
+    def count(self):
+        counter = 1
+        if self.left:
+            counter += self.left.count()
+        if self.right:
+            counter += self.right.count()
+        return counter
+
+    def search(self, data):
+        if self.data == data:
+            return self
+        if data < self.data:
+            if self.left:
+                return self.left.search(data)
+        else:
+            if self.right:
+                return self.right.search(data)
+        return None
+
+    def remove(self, data):  # self na pewno istnieje
+        # Są lepsze sposoby na usuwanie.
+        if data < self.data:
+            if self.left:
+                self.left = self.left.remove(data)
+        elif self.data < data:
+            if self.right:
+                self.right = self.right.remove(data)
+        else:                # self.data == data
+            if self.left is None:   # przeskakuje self
+                return self.right
+            else:     # self.left na pewno niepuste
+                # Szukamy największego w lewym poddrzewie.
+                node = self.left
+                while node.right:   # schodzimy w dół
+                    node = node.right
+                node.right = self.right   # przyczepiamy
+                return self.left
+        return self
+
+    def count_leafs(top):
+        if top is None:
+            return 0
+        if (top.left is None and top.left is None):
+            return 1
+        else: 
+            return Node.count_leafs(top.left) + Node.count_leafs(top.right)
+
+
+    def count_total(top):
+        if top is None:
+            return 0
+        else:
+            return top.data + Node.count_total(top.left) + Node.count_total(top.right)
+
+
+
+
+
+import unittest 
+
+class TestFrac(unittest.TestCase):
+    def setUp(self):
+        self.root = Node(1)
+        self.root.left = Node(2)
+        self.root.right = Node(3)
+        self.root.left.left = Node(4)
+        self.root.left.right = Node(5)
+        
+
+    def test__count_leaf(self):
+        
+        self.assertEqual(Node.count_leafs(self.root), 3)
+
+    def test__count_leaf(self):
+        
+        self.assertEqual(Node.count_total(self.root), 15)   
+
+    def tearDown(self): pass 
+
+if __name__ == '__main__':
+    unittest.main()
+
+
---
+++ Singlelist.py
@@ -0,0 +1,114 @@
+class Node:
+    """Klasa reprezentująca węzeł listy jednokierunkowej."""
+
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+    def __str__(self):
+        return str(self.data)   # bardzo ogólnie
+
+class SingleList:
+    """Klasa reprezentująca całą listę jednokierunkową."""
+
+    def __init__(self):
+        self.length = 0   # nie trzeba obliczać za każdym razem
+        self.head = None
+        self.tail = None
+
+    def is_empty(self):
+        #return self.length == 0
+        return self.head is None
+
+    def count(self):   # tworzymy interfejs do odczytu
+        return self.length
+
+    def insert_head(self, node):
+        if self.head:   # dajemy na koniec listy
+            node.next = self.head
+            self.head = node
+        else:   # pusta lista
+            self.head = self.tail = node
+        self.length += 1
+
+    def insert_tail(self, node):   # klasy O(1)
+        if self.head:   # dajemy na koniec listy
+            self.tail.next = node
+            self.tail = node
+        else:   # pusta lista
+            self.head = self.tail = node
+        self.length += 1
+
+    def remove_head(self):          # klasy O(1)
+        if self.is_empty():
+            raise ValueError("pusta lista")
+        node = self.head
+        if self.head == self.tail:   # self.length == 1
+            self.head = self.tail = None
+        else:
+            self.head = self.head.next
+        node.next = None   # czyszczenie łącza
+        self.length -= 1
+        return node   # zwracamy usuwany node
+
+    def remove_tail(self):
+        if self.is_empty():
+            raise ValueError("pusta lista")
+        node = self.tail
+        if self.head == self.tail:   # self.length == 1
+            self.head = self.tail = None
+        else:
+            while(self.head.next.next):
+                self.head = self.head.next
+            self.head.next = None
+            return node
+
+    def merge(self,other):
+        if self.is_empty() and other.is_empty():
+            return None
+        elif self.is_empty():
+            return other
+        elif other.is_empty():
+            return self
+        else:
+            self.tail.next = other.head
+            self.tail = other.tail
+            self.length = self.length + other.length
+
+    def clear(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+import unittest
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.list1, self.list2  = SingleList(), SingleList()
+        self.list1.insert_head(Node(1))
+        self.list1.insert_head(Node(2))
+        self.list1.insert_head(Node(3))
+        self.list1.insert_head(Node(4))
+        self.list2.insert_head(Node(5))
+        self.list2.insert_head(Node(6))
+        self.list2.insert_head(Node(7))
+
+    def test_remove_tail(self):
+        self.assertEqual(self.list1.remove_tail().data, 1)
+        
+
+    def test_merge(self):
+        self.list1.merge(self.list2)
+        self.assertEqual(self.list1.length, 7)
+        self.assertEqual(self.list1.head.data, 4)
+        self.assertEqual(self.list1.tail.data, self.list2.tail.data)
+       
+
+    def test_clear(self):
+        self.list1.clear()
+        self.assertEqual(self.list1.head, None)
+        self.assertEqual(self.list1.tail, None)
+        self.assertEqual(self.list1.length, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
